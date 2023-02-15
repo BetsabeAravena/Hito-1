9d6d47df98bc71fccf5cb54c41c4782f2b99c98c
Sun aravena <125310964+BetsabeAravena@users.noreply.github.com>
2023-02-15 16:39:42 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-<script>
-cont $answerOne = document.getElementById("answer1"),
-  $answerOne = document.getElementById("1");
-  $answerOne = document.getElementById("2");
-  $answerOne = document.getElementById("3");
 
- $answerOne.addEventListerner("click", function onclick(event) [
-  if ($inputThree.checked)[
+const $answerOne = document.getElementById("answer1"),
+  $inputOne = document.getElementById("one");
+  $inputtwo = document.getElementById("two");
+  $inputThree = document.getElementById("three");
+
+ $answerOne.addEventListerner("click", function onclick(event) {   
+  if ($inputThree.checked) {   
   alert("Marcaste 6 patas, respesta Incorrecta!!");
-  ] else if($inputTwo.checked)[
+  } else if($inputTwo.checked) {   
   alert("Marcaste 12 patas, respuesta Incorrecta!!");
-  ] else if ($inputOne.checked)[
+  } else if ($inputOne.checked) {   
   alert("Marcaste 8 patas,respuesta Correcta!!");
-  ] else [
+    }   else {   
   alert("por favor, selecciona una respuesta");
-  ]
- ]);
+    }
+ });
 
   const $answerTwo=document.getElemtById("answer2"),
   $inputFour= document.getElementById("four"),
-  $inputFour= document.getElementById("five"),
-  $inputFour= document.getElementById("six"),
+  $inputFive= document.getElementById("five"),
+  $inputsix= document.getElementById("six"),
 
-  $answerOne.addEventListerner("click", function onclick(event) [
-  if ($inputfive.checked)[
+  $answerTwo.addEventListerner("click", function onclick(event) {   
+  if ($inputfive.checked){   
   alert("Marcaste Colibri, respesta Correcta!!");
-  ] else if($inputfour.checked)[
+    } else if($inputfour.checked){  
   alert("Marcaste Golondrina, respuesta Incorrecta!!");
-  ] else if ($inputsix.checked)[
+   } else if ($inputsix.checked){   
   alert("Marcaste loro,respuesta Incorrecta!!");
-  ] else [
+  } else {   
   alert("por favor, selecciona una respuesta");
-  ]
- ]);
-</script>
+    }
+   });
