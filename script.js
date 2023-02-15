39a6e74617527cd141e29f4da0f2ea9994c96fc4
Sun aravena <125310964+BetsabeAravena@users.noreply.github.com>
2023-02-15 06:23:35 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+<script>
+cont $answerOne = document.getElementById("answer1"),
+  $answerOne = document.getElementById("1");
+  $answerOne = document.getElementById("2");
+  $answerOne = document.getElementById("3");
+
+ $answerOne.addEventListerner("click", function onclick(event) [
+  if ($inputThree.checked)[
+  alert("Marcaste 6 patas, respesta Incorrecta!!");
+  ] else if($inputTwo.checked)[
+  alert("Marcaste 12 patas, respuesta Incorrecta!!");
+  ] else if ($inputOne.checked)[
+  alert("Marcaste 8 patas,respuesta Correcta!!");
+  ] else [
+  alert("por favor, selecciona una respuesta");
+  ]
+ ]);
+
+  const $answerTwo=document.getElemtById("answer2"),
+  $inputFour= document.getElementById("four"),
+  $inputFour= document.getElementById("five"),
+  $inputFour= document.getElementById("six"),
+
+  $answerOne.addEventListerner("click", function onclick(event) [
+  if ($inputfive.checked)[
+  alert("Marcaste Colibri, respesta Correcta!!");
+  ] else if($inputfour.checked)[
+  alert("Marcaste Golondrina, respuesta Incorrecta!!");
+  ] else if ($inputsix.checked)[
+  alert("Marcaste loro,respuesta Incorrecta!!");
+  ] else [
+  alert("por favor, selecciona una respuesta");
+  ]
+ ]);
+</script>
